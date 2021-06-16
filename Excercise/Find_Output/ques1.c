d8a522f7d275e75adf649f6f2a6f4154644b2550
AmanRaj0904 <aman1462001@gmail.com>
2021-06-16 09:56:20 +0530

    First_Commit

---
+++ Excercise/Find_Output/ques1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int x=4,y,z;
+    y=--x;
+    z=x--;
+    printf("%d\n",x);
+    printf("%d\n",y);
+    printf("%d\n",z);
+    
+    
+    return 0;
+}
--- Excercise/practice.c
+++ Excercise/practice.c
@@ -1,43 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    int first_angle,second_angle,thrid_angle,sum_angle,larg;
-    printf("Enter The First Angle");
-    scanf("%d",&first_angle);
-    printf("Enter The Second Angle");
-    scanf("%d",&second_angle);
-    printf("Enter The third Angle");
-    scanf("%d",&thrid_angle);
+    int i = 1;
+    while (i<=10);
+    {
+        printf("%d\n   hey",i);
+        i++;
+    }
     
-    if(first_angle>second_angle)
-    {
-        printf("First angle is greater \n");
-        larg=first_angle;
-    }
-    else if(second_angle>thrid_angle)
-    {
-        printf("Second angle is greatest \n");
-        larg=second_angle;
-
-    }
-    else
-    {
-        printf("Third Angle is Greatest \n");
-        larg=thrid_angle;
-    }
-
-// printf("%d\n",larg);
-
-    sum_angle=first_angle+second_angle;
-    if(sum_angle>larg)
-    {
-        printf("triangle is Valid");
-    }
-    else
-    {
-        printf("Triangle is Not valid");
-    }
-
-   
     return 0;
 }
