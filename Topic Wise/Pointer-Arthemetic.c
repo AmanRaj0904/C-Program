48e2cdb987a8a5ba53a54a9ede8b630242af52af
AmanRaj0904 <aman1462001@gmail.com>
2021-07-29 15:41:38 +0530

    Pointer Arithmetic

--- Topic Wise/Pointer-Arthemetic.c
+++ Topic Wise/Pointer-Arthemetic.c
@@ -11,5 +11,6 @@ int main()
     int *ptr = &a;
     printf("%d\n\n", ptr);
     printf("%d\n\n", ptr+1);
+    // by incrementing Pointer or Decrementing the Value of pointer is not incrementing or decrementing the Address is incrementing  Decrementing  as per the data type size if we write (ptr++) then it is incremented by type of data-type int has 4byte or vice versha.
     return 0;
 } 
