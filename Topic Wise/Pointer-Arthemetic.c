e6b5432be40c484e0d879b102955c353828ec6ec
AmanRaj0904 <aman1462001@gmail.com>
2021-07-29 15:35:44 +0530

    Pointer Arithmetic

---
+++ Topic Wise/Pointer-Arthemetic.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    // pointer arithemetic 
+    // ++
+    // --
+    // +
+    // -
+    // These all operator are used in Pointer arthemetic
+    int a = 34;
+    int *ptr = &a;
+    printf("%d\n\n", ptr);
+    printf("%d\n\n", ptr+1);
+    return 0;
+} 
