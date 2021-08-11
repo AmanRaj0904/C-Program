120b705ad8be49698e9c9f0bf3f138a3007c3419
AmanRaj0904 <aman1462001@gmail.com>
2021-08-11 16:20:56 +0530

    TYPE_DEF

---
+++ Topic Wise/TYPEDEF_KEYWORD.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    // typedef <Previous_Name> <Alias_Name>
+    typedef unsigned long ul; //Here Type Def Will Change The name of Unsigned long to Ul .it is Used To Give New Name
+    ul l1, l2, l3;
+    typedef int integer;
+    integer a,b,c; //Here New NAme is Given To Int To i    
+    return 0;
+}
