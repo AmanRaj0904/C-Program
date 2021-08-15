457eee3a3fcef9b64307ac66b192cf89f9d3a08b
AmanRaj0904 <aman1462001@gmail.com>
2021-08-15 16:29:44 +0530

    DYNAMIC MEMORY ALLOCATION

---
+++ Topic Wise/DYNAMIC_MEMORY_ALLOCATION.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    // use This Command in Terminal
+    // gcc DYNAMIC_MEMORY_ALLOCATION.c;size .\a.exe
+    // size command tells us size in byte  of TEXT,DATA,BSS,DEC,HEX and total memory taken
+
+//      text    data     bss     dec     hex filename
+// SIZE :   8788    1532    1004   11324    2c3c .\a.exe
+    return 0;
+}
