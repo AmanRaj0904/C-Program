f4bd345419e6c2cd30eedb117aee120353a393db
AmanRaj0904 <aman1462001@gmail.com>
2021-08-16 14:30:26 +0530

    CALLOC FUNCTION

---
+++ Topic Wise/CALLOC_FUNCTION.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h> //Must Use Before The  use of Dynamic Memory Allocation Function
+int main()
+{
+    //use of CALLOC
+    // initially in calloc memory is initilize with Zero 0 value
+    int *ptr;
+    int n;
+    // Allocating Memory on Runtime
+    printf("Enter The Size od Array\n");
+    scanf("%d", &n); //Runtime Allocation
+    ptr = (int *)calloc(n , sizeof(int));
+    // Entering The Value  in the dynamic array Using Loop
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the Value At %d Location \n", i);
+        scanf("%d", &ptr[i]);
+    }
+    // printing The Value of Dyanamic Array Through Loop
+    for (int i = 0; i < n; i++)
+    {
+        printf("The Value At Location %d is = %d\n", i, ptr[i]);
+    }
+    // if We exceed the Size of Loop Then It Will Return Garbage Values
+    return 0;
+}
